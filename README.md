00c0a80c0773dd40e387584e3c61e02cf166dd4f
udaygirish <einsteingirish@gmail.com>
2024-04-27 21:35:14 -0400

    Working ORCA Additions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Python-MCCA
 Python_MCCA Implementation - Pygame
+
+
+
+### Some Random notes
+
+* MCCA
+* ORCA
+* VO
+* GeoPro -VO
--- lib/new_ORCA.py
+++ lib/new_ORCA.py
@@ -1,223 +1,229 @@
-import pygame 
-import numpy as np 
-from scipy.optimize import linprog 
+"""
+ORCA Implementation 
+Random
+
+NON  Smooth
+ORCA + VO Constraint Implemented
+
+"""
+
+import pygame
+import numpy as np
+from scipy.optimize import linprog
 from scipy.spatial import KDTree
 import math
 
-# SCREEN DIMENSIONS
-WIDTH = 800
-HEIGHT = 800
-
-# Define colors
-BLACK = (0, 0, 0)
+# Constants
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+FPS = 60
 
-class ORCARobot:
-    def __init__(self, radius, max_speed, spawn_location, goal_location, time_step, id , color):
+IS_VO = True
+
+class Robot:
+    def __init__(self, radius, max_speed, spawn_location, goal_location, time_step, id, color):
         self.current_location = np.array(spawn_location, dtype=float)
         self.velocity = np.array([0, 0], dtype=float)
         self.goal = np.array(goal_location, dtype=float)
         self.radius = radius
         self.max_speed = max_speed
         self.time_step = time_step
-        self.neighbors = []
+        self.neighbours = []
+        self.AV = []  # Added attribute for velocities
         self.ID = id
         self.color = color
-        self.path = [self.current_location.copy()]  # Initialize path with current location
-    
-    def compute_new_velocity(self, robots, kdtree, radius, time_horizon, max_velocity_change):
-        # Query KD-tree to find nearest neighbors within the radius
-        neighbors_indices = kdtree.query_ball_point(self.current_location, radius)
-        # Filter out the current robot's index
-        neighbors_indices = [idx for idx in neighbors_indices if idx != self.ID]
-        # Get the nearest neighbor robots
-        nearest_neighbors = [robots[idx] for idx in neighbors_indices]
-        num_neighbors = len(nearest_neighbors)
-        
-        num_dims = len(self.current_location)
-        
-        # Construct the Linear Programming problem 
-        c = np.zeros(num_dims)
-        A = np.zeros((2*num_neighbors, num_dims))
-        b = np.zeros(2*num_neighbors)
-        
-        #print("Robot: ", self.ID)
-        #print("Length of Nearest Neighbors: ", len(nearest_neighbors))
-        for i, robot in enumerate(nearest_neighbors):
-            relative_position = robot.current_location - self.current_location
-            relative_velocity = np.array(robot.velocity) - self.velocity
-            dist = np.linalg.norm(relative_position)
-            time_to_collision = - np.dot(relative_position, relative_velocity) / (dist**2)
-            
-            time_to_collision = max(time_to_collision, 0)
-            print("Time to Collision: ", time_to_collision)
-            # if time_to_collision < 0:
-            #     continue
-            
-            # Limit the time horizon
-            print(f"Robot {self.ID} - Time to Collision Before: {time_to_collision}")
-            time_to_collision = min(time_to_collision, time_horizon)
-            print(f"Robot {self.ID} - Time to Collision After: {time_to_collision}")
-            #  Compute Velocity constraints
-            v_pref = robot.max_speed * relative_position / dist
-            u = (v_pref - self.velocity) / np.linalg.norm(v_pref - self.velocity)
-            perpendicular = np.array([-u[1], u[0]])
-            
-            # Adjust the radius of influence
-            radius_of_influence =4 * (self.radius + robot.radius)
-            
-            # Construct ORCA half-plane constraints
-            A[i] = perpendicular
-            b[i] = np.dot(perpendicular, self.current_location + self.velocity * time_to_collision + 0.5 * u * (time_to_collision**2)) + radius_of_influence - dist 
-            
-            A[i + num_neighbors] = -perpendicular
-            b[i + num_neighbors] = -np.dot(-perpendicular, self.current_location + self.velocity * time_to_collision + 0.5 * u * (time_to_collision**2)) + radius_of_influence - dist
-        
-        #print(f"Robot {self.ID} - A: {A}")
-        #print(f"Robot {self.ID} - b: {b}")
-        # Remove rows with NaN values
-        valid_rows = np.logical_not(np.any(np.isnan(A) | np.isinf(A), axis=1))
-        A = A[valid_rows]
-        b = b[valid_rows]
+        self.trail = [self.current_location.copy()]  # Trail to store previous locations
 
-        
-        # Construct the Linear Programming problem
-        res = linprog(c, A_ub=A, b_ub=b)
+    def is_goal_reached(self):
+        return np.linalg.norm(self.goal - self.current_location) < self.radius
 
-        #print(res.x)
-        #delta_velocity = res.x
-        
-            
-        #print(f"Robot {self.ID} - Res: {res}")
-        # Limit the maximum allowable change in velocity
-        delta_velocity = np.clip(res.x, -max_velocity_change, max_velocity_change)
-        #delta_velocity = res.x
-        print(f"Robot {self.ID} - Delta Velocity: {delta_velocity}")
-        # Update the velocity
-        if np.linalg.norm(delta_velocity) > 0 and time_to_collision > 0:
-            self.velocity = self.velocity + 0.5* delta_velocity
-        # else:
-        #     self.velocity = self.velocity
-        #print(f"Robot {self.ID} - Velocity: {self.velocity}")
-        #return self.velocity
-    
-    def update_position(self):
-        # Update the position based on the current velocity
+    def update_current_location(self):
+        self.trail.append(self.current_location.copy())  # Store the current location in the trail
         self.current_location += self.velocity * self.time_step
-        # Add current location to the path
-        self.path.append(self.current_location.copy())
 
-def draw_robot(robots, screen):
-    for robot in robots:
-        # Draw path
-        if len(robot.path) > 1:
-            pygame.draw.lines(screen, robot.color, False, robot.path, 2)
-        # Draw robot
-        pygame.draw.circle(screen, robot.color, robot.current_location.astype(int), robot.radius)
-        # Draw heading direction
-        pygame.draw.line(screen, robot.color, robot.current_location.astype(int), (robot.current_location + robot.velocity).astype(int), 2)
+    def preferred_velocity(self):
+        if not self.is_goal_reached():
+            direction = self.goal - self.current_location
+            direction_unit_vector = direction / np.linalg.norm(direction)
+            self.pref_vel = self.max_speed * direction_unit_vector
+        else:
+            self.pref_vel = np.zeros(2)
+
+    def calc_vel_penalty(self, robot2, new_vel, omega):
+        new_vel = 2 * new_vel - self.velocity
+        distance = (self.radius + robot2.radius) ** 2
+        a = (new_vel[0] - robot2.velocity[0]) ** 2 + (new_vel[1] - robot2.velocity[1]) ** 2
+        b = 2 * ((self.current_location[0] - robot2.current_location[0]) * (new_vel[0] - robot2.velocity[0]) +
+                 (self.current_location[1] - robot2.current_location[1]) * (new_vel[1] - robot2.velocity[1]))
+        c = (self.current_location[0] - robot2.current_location[0]) ** 2 + (self.current_location[1] -
+                                                                            robot2.current_location[1]) ** 2 - distance
+        d = b ** 2 - 4 * a * c
+        if b > -1e-6 or d <= 0:
+            return np.linalg.norm(self.pref_vel - new_vel)
+        e = math.sqrt(d)
+        t1 = (-b - e) / (2 * a)
+        t2 = (-b + e) / (2 * a)
+        if t1 < 0 < t2 and b <= -1e-6:
+            return 0
+        else:
+            time_to_col = t1
+        penalty = omega * (1 / time_to_col) + np.linalg.norm(self.pref_vel - new_vel)
+        return penalty
+
+    def get_neighbours(self, kd_tree, all_robots, radius):
+        self.neighbours = []
+        neighbours_indices = kd_tree.query_ball_point(self.current_location, radius)
+        for i in neighbours_indices:
+            if all_robots[i] != self:
+                self.neighbours.append(all_robots[i])
+
+    def useable_velocities(self, combined_VO):
+        self.AV = [vel for vel in self.velocities if tuple(vel) not in map(tuple, combined_VO)]
+
+    def choose_velocity(self, combined_RVO, VO=False):
+        self.preferred_velocity()
+        VO = IS_VO
+        if (VO):
+            optimal_vel = self.pref_vel
+        if not self.AV:
+            min_penalty = float('inf')
+            for neighbour in self.neighbours:
+                for vel in combined_RVO:
+                    penalty = self.calc_vel_penalty(neighbour, vel, omega=1)
+                    if penalty < min_penalty:
+                        min_penalty = penalty
+                        optimal_vel = vel
+        else:
+            min_closeness = float('inf')
+            for vel in self.AV:
+                closeness = np.linalg.norm(self.pref_vel - vel)
+                if closeness < min_closeness:
+                    min_closeness = closeness
+                    optimal_vel = vel
+        if (VO):
+            self.velocity = optimal_vel
+        else:
+            self.velocity = (optimal_vel + self.velocity) / 2
+
+    def compute_VO_and_RVO(self, robot2):
+        VO = []
+        RVO = []
+        for vel in self.velocities:
+            constraint_val = self.collision_cone_val(vel, robot2)
+            if constraint_val < 0:
+                VO.append(vel)
+                RVO.append((vel + self.velocity) / 2)
+        return VO, RVO
+
+    def compute_combined_RVO(self, neighbour_robots):
+        combined_VO = []
+        combined_RVO = []
+        for neighbour_robot in neighbour_robots:
+            combined_VO.extend(self.compute_VO_and_RVO(neighbour_robot)[0])
+            combined_RVO.extend(self.compute_VO_and_RVO(neighbour_robot)[1])
+        return combined_VO, combined_RVO
+
+    def collision_cone_val(self, vel, robot2):
+        rx = self.current_location[0]
+        ry = self.current_location[1]
+        vrx = vel[0]
+        vry = vel[1]
+        obx = robot2.current_location[0]
+        oby = robot2.current_location[1]
+        vobx = robot2.velocity[0]
+        voby = robot2.velocity[1]
+        R = self.radius + robot2.radius + 10
+        constraint_val = -((rx - obx) * (vrx - vobx) + (ry - oby) * (vry - voby)) ** 2 + (
+                -R ** 2 + (rx - obx) ** 2 + (ry - oby) ** 2) * ((vrx - vobx) ** 2 + (vry - voby) ** 2)
+        return constraint_val
+
+    def draw(self, screen):
+        if len(self.trail) >= 2:
+            pygame.draw.lines(screen, self.color, False, self.trail, 2)
+        pygame.draw.circle(screen, self.color, (int(self.current_location[0]), int(self.current_location[1])),
+                           self.radius)
+        end_point = self.current_location + 10 * self.velocity
+        pygame.draw.line(screen, (0, 0, 0), self.current_location, end_point, 2)
+        pygame.draw.circle(screen, self.color, (int(self.goal[0]), int(self.goal[1])), 3)
         
 def main():
     pygame.init()
-    
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("ORCA Algorithm")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("RVO + ORCA")
     clock = pygame.time.Clock()
-    
-    # Define Simulation params 
+
+    # Simulation parameters
+    num_robots = 6
     time_horizon = 2.0
     time_step = 0.1
-    max_velocity_change = 3 # Maximum allowable change in velocity
-    
-    num_robots = 6
-    radius_of_influence = 100   # Define Robots influence
-    robots = []
-    
+    max_velocity_change = 3
+    IS_VO = True  # Set True for VO and False for RVO
+
     # Define circle parameters
-    circle_center = np.array([WIDTH // 2, HEIGHT // 2])
-    circle_radius = min(WIDTH, HEIGHT) // 3
-    
+    circle_center = np.array([SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2])
+    circle_radius = min(SCREEN_WIDTH, SCREEN_HEIGHT) // 3
+
     # Define colors for each robot
     robot_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255)]
-    
+
     # Spawn robots evenly spaced on the circumference of a circle
+    robots = []
     for i in range(num_robots):
         angle = 2 * math.pi * i / num_robots
         spawn_x = circle_center[0] + circle_radius * math.cos(angle)
         spawn_y = circle_center[1] + circle_radius * math.sin(angle)
-        
+
         # Define opposite direction goal positions
         goal_x = circle_center[0] + circle_radius * math.cos(angle + math.pi)
         goal_y = circle_center[1] + circle_radius * math.sin(angle + math.pi)
-        
-        # print(f"Robot {i} - Spawn: ({spawn_x}, {spawn_y}) - Goal: ({goal_x}, {goal_y})")
-        # radius, max_speed, spawn_location, goal_location, time_step, id , color
-        robots.append(ORCARobot(10, max_velocity_change, [spawn_x, spawn_y], [goal_x, goal_y], time_step, i, robot_colors[i]))
-    
-    # Build KD-tree
-    robot_locations = np.array([robot.current_location for robot in robots])
-    kdtree = KDTree(robot_locations)
-    
-    # Debug
-    # Print current positions 
-    # for robot in robots:
-    #     print(f"Robot {robot.ID} - Current Location: {robot.current_location}")
-        
-    # # Print Spawn and Goal locations 
-    # for robot in robots:
-    #     print(f"Robot {robot.ID} - Spawn: {robot.current_location} - Goal: {robot.goal}")
-    
-    
+
+        robots.append(Robot(10, max_velocity_change, [spawn_x, spawn_y], [goal_x, goal_y], time_step, i,
+                            robot_colors[i]))
+
     running = True
-    
     count = 0
     max_count = 1e5
-    
-    # Set max speed for each robot initially towards the goal
-    for robot in robots:
-        direction = robot.goal - robot.current_location
-        distance = np.linalg.norm(direction)
-        if distance > 1:
-            robot.velocity = (direction / distance) * robot.max_speed
-            
-    # Print initial velocities
-    for robot in robots:
-        print(f"Robot {robot.ID} - Initial Velocity: {robot.velocity}")
-        
+
     while running:
         if count > max_count:
             running = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        # Build KD-tree
         robot_locations = np.array([robot.current_location for robot in robots])
         kdtree = KDTree(robot_locations)
-        # Update robot positions towards the goal
+
         for robot in robots:
-            # Calculate direction towards the goal
-            direction = robot.goal - robot.current_location
-            distance = np.linalg.norm(direction)
-            if distance > 1:  # Check if the robot has reached its goal
-                # Move towards the goal at maximum speed
-                robot.velocity = (direction / distance) * robot.max_speed 
-                print("Robot: ", robot.ID, "Distance: ", distance, "Direction: ", direction, "Velocity: ", robot.velocity)
-                #print("Distance: ", distance, "Direction: ", direction, "Velocity: ", robot.velocity)
-            
+            robot.get_neighbours(kdtree, robots, 100)
+
         for robot in robots:
-            if distance > 0.2:
-                robot.compute_new_velocity(robots, kdtree, radius_of_influence, time_horizon, max_velocity_change)
-                robot.current_location += robot.velocity * time_step
-                # Add current location to the path
-                robot.path.append(robot.current_location.copy())
-                
-        screen.fill(BLACK)
-        draw_robot(robots, screen)
+            robot.velocities = []
+            for i in range(0, 360, 15):
+                angle = math.radians(i)
+                x = math.cos(angle) * robot.max_speed
+                y = math.sin(angle) * robot.max_speed
+                robot.velocities.append(np.array([x, y]))
+
+        for robot in robots:
+            combined_VO, combined_RVO = robot.compute_combined_RVO(robot.neighbours)
+            robot.useable_velocities(combined_VO)
+            robot.choose_velocity(combined_RVO, IS_VO)
+
+        for robot in robots:
+            robot.update_current_location()
+
+        screen.fill(WHITE)
+        for robot in robots:
+            robot.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        
-        # Increment the count
-        count = count + 1
-        
+        clock.tick(FPS)
+        count += 1
+
     pygame.quit()
 
 if __name__ == "__main__":
